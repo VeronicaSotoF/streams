793629fb4ac6921a4b9ec2f4ad4882bc64622b20
VeronicaSotoF <sotofierroveronicadev@gmail.com>
2022-12-01 11:34:51 -0600

    first commit on new branch

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -58,7 +58,7 @@ class StreamList extends React.Component {
   render() {
     return (
       <div>
-        <h2>Streams</h2>
+        <h2>Streams V2.0</h2>
         <div className="ui celled list">{this.renderList()}</div>
         {this.renderCreate()}
       </div>
